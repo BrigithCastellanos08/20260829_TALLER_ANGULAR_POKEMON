62a96c881ecb451fb307743d9a0673922b6c412c
Brigith Castellanos <bridanacasmar222@gmail.com>
2026-09-08 01:56:45 +0000

    cambios

--- BuscadorPokemon/src/app/components/registro-usuario/registro-usuario.ts
+++ BuscadorPokemon/src/app/components/registro-usuario/registro-usuario.ts
@@ -1,115 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, signal  } from '@angular/core';
+import{FormsModule} from '@angular/forms';
+
+export interface Usuario {
+  id : number;
+        nombrecompleto : string;
+        documento : { 
+            tipo : string; 
+            numero : string;
+        }
+        fecha_nacimiento : string;
+        correo_electronico : string;
+        domicilio : {
+             pais_domicilio : string;
+             ciudad_domicilio : string;
+        }
+
+        Número_de_teléfono : string;
+        tratamiento_datos : boolean;
+}
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-registro-usuario',
+  standalone : true,
   styleUrl: './registro-usuario.css',
   templateUrl: './registro-usuario.html',
 })
 export class RegistroUsuario {
-}
-function CrearUsuario ( nombre,apellido,tipo_documento,documento,fecha_nacimiento,correo,pais,ciudad,telefono,tratamiento_datos){
+    nombre = signal('');
+    apellido = signal('');
+    tipo_documento = signal('CC');
+    documento = signal('');
+    fecha_nacimiento = signal('');
+    correo = signal('');
+    pais = signal('');
+    cuidad = signal('');
+    telefono= signal('');
+    tratamiento_datos = signal(false);
 
-    const UsuarioCreado = {
+    ultimoUsuario =signal<Usuario | null>(null);
+    
+    guardarUsuario(){
+        if(!this.tratamiento_datos()){
+            alert('Debes aceptar el tratamiento de datos personales'); 
+        }
+        const usuarioCreado = {
         id : Date.now(),
-        nombrecompleto : `${nombre} ${apellido}`,
+        nombrecompleto : `${this.nombre()} ${this.apellido()}`,
         documento : { 
-            tipo : tipo_documento, 
-            numero : documento
+            tipo : this.tipo_documento(), 
+            numero : this.documento()
         },
-        fecha_nacimiento : fecha_nacimiento,
-        correo_electronico : correo,
+        fecha_nacimiento : this.fecha_nacimiento(),
+        correo_electronico : this.correo(),
         domicilio : {
-             pais_domicilio : pais,
-             ciudad_domicilio : ciudad
+             pais_domicilio : this.pais(),
+             ciudad_domicilio : this.cuidad()
         },
 
-        Número_de_teléfono : telefono,
-        tratamiento_datos : tratamiento_datos
-        
-        
-    
-    
+        Número_de_teléfono : this.telefono(),
+        tratamiento_datos : this.tratamiento_datos()
+           
     }
-    return UsuarioCreado 
-        
-    }
-  
-
-//captura de informacionA
-const formulario = document.querySelector('form');
-
-
-formulario.addEventListener('submit',function(event) {
-    event.preventDefault();
-
-    const Nombre =document.getElementById('nombre').value;
-    const Apellido =document.getElementById('apellido').value;
-    const tipoDocumento =document.getElementById('tipo_documento').value;
-    const numeroDocumento =document.querySelector('.documento-field input')?.value || '';
-    const fechaNacimiento =document.getElementById('fecha_nacimiento').value;
-    const correoElectronico =document.getElementById('correo').value;
-    const Pais =document.getElementById('pais').value;
-    const Ciudad =document.querySelector('.ciudad-field select')?.value || '';
-    const numeroTelefono =document.getElementById('telefono').value;
-    const tratamientoDatos =document.getElementById('tratamiento_datos').checked;
-
-    const CrearUsuario2 = CrearUsuario(Nombre,Apellido,tipoDocumento,numeroDocumento,fechaNacimiento,correoElectronico,Pais,Ciudad,numeroTelefono,tratamientoDatos);
-    
-    //convertir los datos en JSON 
-    const usuarioJSON = JSON.stringify(CrearUsuario2)
-
-    //Guardado 
-    localStorage.setItem(CrearUsuario2.id,usuarioJSON)
-
-});
-
-
-const formularioPokemon = document.getElementById('formPokedex');
-
-formularioPokemon.addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const nombrePokemon = document.getElementById('pokemonInput').value.trim().toLowerCase();
-
-    if(!nombrePokemon) return;
-
-try{
-
-    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
-
-    if(!respuesta.ok) {
-        throw new Error ('¡No encontre nada chamo!');
-
-}
-
-   const datos = await respuesta.json();
-   
-   cositasPokemon(datos);
-}
-
-
- catch(error) {
-    const contenedor = document.getElementById('resultadoPokemon')
-    contenedor.innerHTML =
-      `<div> 
-        <p>  ${error.message}</p>
-      </div>`;
- }
-});
-
-function cositasPokemon(argumentos){
-
-    const contenedor = document.getElementById('resultadoPokemon')
-    const nombre = argumentos.name.toUpperCase();
-    const imagen = argumentos.sprites.front_default;
-    contenedor.innerHTML =
-      `<div> 
-        <h2>${nombre}</h2>
-        <img src ="${imagen}" style="with: 200px; height: 200px">
-     </div>`;
-
-}
+    localStorage.setItem(usuarioCreado.id.toString(),JSON.stringify(usuarioCreado));
     
 
-
-    
+    this.ultimoUsuario.set(usuarioCreado);
+}
+}
