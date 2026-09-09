122a4b11df15f222492bba70dd1fb8d7b26fec3b
Brigith Castellanos <bridanacasmar222@gmail.com>
2026-09-09 02:02:25 +0000

    cambios

--- BuscadorPokemon/src/app/components/buscador-pokemon/buscador-pokemon.ts
+++ BuscadorPokemon/src/app/components/buscador-pokemon/buscador-pokemon.ts
@@ -1,4 +1,4 @@
-import { Component, signal  } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 interface PokemonData {
@@ -17,35 +17,39 @@ interface PokemonData {
 
 
 export class BuscadorPokemon {
-  nombrePokemonInput =signal('') ;
+  nombrePokemonInput = signal('') ;
   pokemon = signal<PokemonData | null>(null);
   mensajeError = signal<string | null>(null);
 
   async buscarPokemon() {
-    const nombrePokemon = this.nombrePokemonInput().trim().toLowerCase();
+  
+  const nombrePokemon = this.nombrePokemonInput().trim().toLowerCase();
 
-    if (!nombrePokemon) return;
+  if (!nombrePokemon) return;
 
-    this.mensajeError.set(null);
-    this.pokemon.set(null);
+  this.mensajeError.set(null); // Limpiar mensaje de error antes de la búsqueda
+  
+  try {
+    
+    const  respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
 
-    try {
-      const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
-
-      if (!respuesta.ok) {
-        throw new Error('¡No se encontro el Pokemon!');
-      }
-
-      const datos = await respuesta.json();
-
-      this.pokemon.set({
-        name: datos.name,
-        image: datos.sprites?.front_default ?? '',
-        type: datos.types?.map((typeInfo: any) => typeInfo.type.name).join(', ') ?? '',
-      });
-    } catch (error) {
-      this.mensajeError.set(error instanceof Error ? error.message : 'Error al buscar el Pokémon.');
-      this.pokemon.set(null);
+    if (!respuesta.ok) {
+    throw  new Error('¡No se encontró el Pokémon!');
     }
-  } 
+
+    const datos = await respuesta.json();
+
+    this.pokemon.set({
+      name: datos.name.toUpperCase(),
+      image: datos.sprites.front_default,
+      type: datos.types.map((typeInfo: any) => typeInfo.type.name).join (', '), //EL MAP ME CONVIERTE EN TEXTO DE ACUERDO CON LA PROPIEDAD QUE YO LE PIDA EN ESTE CASO ES NAME//ME LLAMA LISTAS
+    });
+
+  }catch (error: any) {
+    this.pokemon.set(null);
+    this.mensajeError.set(error.message);
+  }
+  
+}
+
 }
